8fea8cfccfa74ec3aa725e8f9d4feea93e691eb8
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-06-23 10:04:01 +0200

    update compo

--- cinetag/src/pages/Favoritos/index.js
+++ cinetag/src/pages/Favoritos/index.js
@@ -1,9 +1,16 @@
-import style from "./Favoritos.module.css"
+import Banner from "../../components/Banner";
+import Card from "../../components/Card";
+import Titulo from "../../components/Titulo";
+import styles from "./Favoritos.module.css"
 
 function Favoritos() {
     return (
         <>
-        
+        <Banner imagem="favoritos"/>
+        <Titulo>Meus Favoritos</Titulo>
+        <section className={styles.container}>
+            <Card></Card>
+        </section>
         </>
     )
 }
--- cinetag/src/routes.js
+++ cinetag/src/routes.js
@@ -1,22 +1,24 @@
 import Container from "./components/Container";
+import FavoritosProvider from "./contextos/Favoritos";
 import Favoritos from "./pages/Favoritos";
 import Inicio from "./pages/Inicio";
-import { BrowserRouter, Route, Routes} from "react-router-dom"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function AppRoutes() {
-    return (
-        <BrowserRouter>
-        <Cabecalho />
-            <Container >
-            <Routes>
-                <Route path="/" element={<Inicio />}></Route>
-                <Route path="/Favoritos" element={<Favoritos />}></Route>
-            </Routes>
-            </Container>
-        <Rodape />
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Cabecalho />
+      <Container>
+        <FavoritosProvider>
+          <Routes>
+            <Route path="/" element={<Inicio />}></Route>
+            <Route path="/Favoritos" element={<Favoritos />}></Route>
+          </Routes>
+        </FavoritosProvider>
+      </Container>
+      <Rodape />
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
